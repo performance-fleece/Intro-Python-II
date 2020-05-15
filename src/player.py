5a78382e4eace9a8a6148f0e085f4f4ede413310
Jeremy Morales <jeremy.morales@gmail.com>
2020-05-14 21:19:40 -0700

    minor player format changes

--- src/player.py
+++ src/player.py
@@ -3,51 +3,15 @@
 
 
 class Player:
-    def __init__(self, name, room=["outside"], items=[]):
+    default_start = "outside"
+
+    def __init__(self, name, room=default_start, items=[]):
         self.name = name
         self.room = room
         self.items = items
 
     def __str__(self):
         return f"{self.name} is in {self.room}"
-
-    # def travel(self, player_input):
-    #     if player_input == 'n':
-    #         print('You attempt to travel' +
-    #               '\033[1;32;40m north \033[0;37;40m')
-    #         try:
-    #             self.room = self.room.n_to
-
-    #         except:
-    #             print('Error moving north')
-
-    #     if player_input == 'e':
-    #         print('You attempt to travel' + '\033[1;32;40m east \033[0;37;40m')
-
-    #         try:
-    #             self.room = self.room.e_to
-
-    #         except:
-    #             print('Error moving east')
-
-    #     if player_input == 's':
-    #         print('You attempt to travel' +
-    #               '\033[1;32;40m south \033[0;37;40m')
-
-    #         try:
-    #             self.room = self.room.s_to
-
-    #         except:
-    #             print('Error moving south')
-
-    #     if player_input == 'w':
-    #         print('You attempt to travel' + '\033[1;32;40m west \033[0;37;40m')
-
-    #         try:
-    #             self.room = self.room.w_to
-
-    #         except:
-    #             print('Error moving West')
 
     def travel(self, player_input):
         try:
